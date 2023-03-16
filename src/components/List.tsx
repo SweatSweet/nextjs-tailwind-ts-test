21ef99e92179f886a1338fe487d580f099bbb363
Matias <matiastheeravit@gmail.com>
2023-03-16 04:45:22 +0200

    updated padding

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,8 +5,10 @@ interface Props {
 
 export const List = (props: Props) => {
   return (
-    <div className="m-0 py-[12px] pl-[24px] pr-[19.98px] shadow-md bg-white border border-opacity-[0.1] rounded-[8px]">
-      <h3 className="font-semibold text-[20px]">{props.title}</h3>
+    <div className="m-0 pl-[24px] pr-[19.98px] shadow-md bg-white border border-opacity-[0.1] rounded-[8px]">
+      {props.title && (
+        <h3 className="font-semibold text-[20px] py-[12px] ">{props.title}</h3>
+      )}
       {props.children}
     </div>
   );
